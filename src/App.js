1c815f2746a9ca13fb68250b9c89a72bd77ee2a0
Tiyara-03 <tiara26dia37@gmail.com>
2024-06-29 12:20:45 +0530

    update in app.js and package.json

--- src/App.js
+++ src/App.js
@@ -6,30 +6,36 @@ import About from './components/About';
 import { useState } from 'react';
 import Alert from './components/Alert';
 
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 function App() {
-  const [ mode, setMode] = useState('light'); 
-  const[alert, setAlert] = useState(null);  //state for alert
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);  //state for alert
 
 
-  const showAlert = (message, type)=>{       //Method to show alert messages
-     setAlert({ 
+  const showAlert = (message, type) => {       //Method to show alert messages
+    setAlert({
       msg: message,
       type: type
-     })
+    })
 
-     setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-     }, 1000);
+    }, 1000);
   }
 
-  const toggleMode = () =>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark')
       document.body.style.backgroundColor = "rgb(24 35 45)";
       showAlert("Dark Mode Enabled", "success");
 
     }
-    else{
+    else {
       setMode('light')
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode Enables", "success");
@@ -40,13 +46,19 @@ function App() {
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>  */}
-      <Navbar title="textUtils" mode={mode} toggleMode = {toggleMode}/>
-      {/* {alert} it is state variable for passing the value in alert state */}
-      <Alert alert={alert}/>        
-      <div className="container my-3">
-      <Textform showAlert={showAlert} heading=" Enter the text to analyse below" mode={mode}/>
-      </div>
-      <About />
+      <Router>
+        <Navbar title="textUtils" mode={mode} toggleMode={toggleMode} />
+        {/* {alert} it is state variable for passing the value in alert state */}
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path='/'element={<Textform showAlert={showAlert} heading=" Enter the text to analyse below" mode={mode}/> }/>
+            <Route exact path='/about'element={<About />}/>
+          </Routes>
+        </div>
+      </Router>
+
+
 
     </>
   );
